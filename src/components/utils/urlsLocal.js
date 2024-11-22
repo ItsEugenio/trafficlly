f2988cada052cb1cd95c7387124af893af5f3cd1
Eugenio <111914875+ItsEugenio@users.noreply.github.com>
2024-11-22 04:14:21 -0600

    Update urlsLocal.js

--- src/components/utils/urlsLocal.js
+++ src/components/utils/urlsLocal.js
@@ -2,7 +2,7 @@ export const urls = {
     backTrafficlly: 'https://trafficlly.ddns.net/backtrafficlly/',
     backSystem: 'https://trafficlly.ddns.net/backsecurity/',
     webSocketSecurity: 'wss://trafficlly.ddns.net/wstrafficlly/',
-    webSocketTrafficlly: 'wss://trafficlly.ddns.net/wssecurity/',
+    webSocketTrafficlly: 'wss://52.202.11.3:5000',
 }
 
 
